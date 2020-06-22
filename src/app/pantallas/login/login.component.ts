28027af3ae574c7a85d164b879f56c23c3799fad
Jose Orlando Escobar <45208259+joe-hn@users.noreply.github.com>
2020-06-22 15:45:57 -0600

    LOGIN

--- src/app/pantallas/login/login.component.ts
+++ src/app/pantallas/login/login.component.ts
@@ -43,6 +43,7 @@ export class LoginComponent implements OnInit {
           this.noUsuario = true;
         }
       }, error => {
+        console.log(error);
         this.router.navigate(['/error']);        
       })
 
